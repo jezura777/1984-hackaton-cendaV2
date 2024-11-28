8e5ae52f0f96d99a68354673280106bcf86ab0f1
Ondřej Zeman <ondrejzeman23@gmail.com>
2024-11-28 13:12:42 +0100

    Init tailwindcss, init nativewind, init global.css file, import global.css to layout

---
+++ client/mobile/tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+	presets: [require("nativewind/preset")],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				mplus: ["'proxima-soft'", 'Verdana', 'sans-serif'],
+			},
+		},
+	},
+} satisfies Config;
