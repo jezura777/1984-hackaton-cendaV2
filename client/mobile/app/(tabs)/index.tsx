a4fe509032a94c9ae25292678c45fae85f81ffbc
Ondřej Zeman <ondrejzeman23@gmail.com>
2024-11-28 21:41:57 +0100

    Implement recent plants component

--- client/mobile/app/(tabs)/index.tsx
+++ client/mobile/app/(tabs)/index.tsx
@@ -5,12 +5,14 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { View, Text } from 'react-native';
-import Header from '../../components/Header';
+import Header from '@/components/Header';
+import RecentPlants from '@/components/RecentPlants';
 
 export default function HomeScreen() {
 	return (
 		<View>
 			<Header />
+			<RecentPlants />
 		</View>
 	);
 }
